0cbf84ebda90060494f74eb325da4b4ca7ca2a08
Sean Smevik <smsmev@gmail.com>
2013-01-08 12:52:21 -0500

    Update js/addressbook.js
    fixed search (maybe!)

--- js/addressbook.js
+++ js/addressbook.js
@@ -5,29 +5,20 @@ $(document).ready(function () {
 $.getJSON('data/contacts.json', function (data) {
     
     var addrBook = data.addressBook,
-        count = addrBook.length
+        count = addrBook.length,
+        queryResult = $("#q").val();
         
         $('#output').empty();
-          
-          if (count > 0) {
                     
         $.each(addrBook, function (i, obj) {
-        
-            $('#output').append('<p>' + obj.name + ', <a href="mailto:' + obj.email + ' ">' + obj.email +'</a><p>');  
             
-            });
-        }
-    }).error(function (){
-    
-        alert('there was and ajax error');
-    
-    }).complete(function () {
+            var isItFound = obj.name.indexOf(queryResult);
             
-        alert('your ajax call is complete');
-    
-    }).success(function() {
-    
-        alert('your ajax call was a success')
+            if isItFound !== -1 {
+                $('#output').append('<p>' + obj.name + ', <a href="mailto:' + obj.email + ' ">' + obj.email +'</a><p>');
+            }
+            
+        });
 
     }); //end ajax
     
